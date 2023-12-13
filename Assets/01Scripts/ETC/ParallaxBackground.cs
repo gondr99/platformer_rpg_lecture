228e85fc0457d305bdde632252b5459a65b0f3af
gondr99 <gondr99@gmail.com>
2023-12-14 08:38:35 +0900

    fix parallax bug

--- Assets/01Scripts/ETC/ParallaxBackground.cs
+++ Assets/01Scripts/ETC/ParallaxBackground.cs
@@ -9,46 +9,57 @@ public class ParallaxBackground : MonoBehaviour
     private Transform _mainCamTrm;
     private Vector2 _initPosition;
     private Vector2 _spriteSize;
-    
+
+    private Vector2 _camInitPosition;
 
     void Awake()
     {
         _mainCamTrm = Camera.main.transform;
+        _camInitPosition = _mainCamTrm.position;
         _initPosition = transform.position; //시작 위치
-
         _spriteSize = GetComponent<SpriteRenderer>().bounds.size;
     }
-    
+
+    private void Start()
+    {
+        
+    }
+
     void LateUpdate()
     {
-        float distanceToMovedX = _mainCamTrm.position.x * (1 - parallaxRatio.x);
-        float distanceToMovedY = _mainCamTrm.position.y * (1 - parallaxRatio.y);
+        MoveToCameraPosition();
+    }
+
+    private void MoveToCameraPosition()
+    {
+        Vector2 cameraDelta = (Vector2)_mainCamTrm.position - _camInitPosition;
+
         
-        Vector2 moveOffset = new Vector2(_mainCamTrm.position.x * parallaxRatio.x, _mainCamTrm.position.y * parallaxRatio.y);
+        Vector2 moveOffset = new Vector2(cameraDelta.x * parallaxRatio.x, cameraDelta.y * parallaxRatio.y);
         transform.position = _initPosition + moveOffset;
+
+        Vector2 deltaFromCam =  (Vector2)_mainCamTrm.position - _initPosition;
         
         //X축에 대한 무한 스크롤 보정
-        if (distanceToMovedX > _initPosition.x + _spriteSize.x) //오른쪽이동.
+        if (deltaFromCam.x > _spriteSize.x) //오른쪽이동.
         {
             _initPosition.x += _spriteSize.x;
         }
-        else if( distanceToMovedX < _initPosition.x - _spriteSize.x)  //왼쪽이동
+        else if (deltaFromCam.x < -  _spriteSize.x)  //왼쪽이동
         {
             _initPosition.x -= _spriteSize.x;
         }
 
         if (_yAxisRepeat)
         {
-            if (distanceToMovedY > _initPosition.y + _spriteSize.y) //위쪽
+            if (deltaFromCam.y >  _spriteSize.y) //위쪽
             {
                 _initPosition.y += _spriteSize.y;
             }
-            else if( distanceToMovedY < _initPosition.y - _spriteSize.y)  //아래쪽
+            else if (deltaFromCam.y <  - _spriteSize.y)  //아래쪽
             {
                 _initPosition.y -= _spriteSize.y;
             }
         }
-
-
     }
 }
